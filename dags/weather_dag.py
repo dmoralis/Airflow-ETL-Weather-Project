4467797c0f0c6a0b11c3f991ca3a27b8e91a480a
Ubuntu <ubuntu@ip-172-31-24-192.eu-north-1.compute.internal>
2023-09-29 08:39:44 +0000

    Add extract_weather_data operator, where we use SimpleHttpOperator to get the data from the weather API if the HttpSensor has gotten a positive answer

--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -1,17 +1,18 @@
 from airflow import DAG
 from datetime import timedelta, datetime
 from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
 import json
 
 
 default_args = {
     'owner':'airflow',
     'depends_on_past':False,
-    'start_date':datetime(2023, 9, 26),
+    'start_date':datetime(2023, 9, 27),
     'email':[],
     'email_on_failure':False,
     'email_on_retry':False,
-    'retries':1,
+    'retries':0,
     'retry_delay':timedelta(minutes=2.0)
 }
 
@@ -21,12 +22,23 @@ with DAG('weather_dag',
     schedule_interval='@daily',
     catchup=False) as dag:
 
-    lat = '40.6403167'
-    lon = '22.9352716'
-    api_key = '800bbfb2f56e6861f7f1ae1bebce4528'
+    #lat = '40.6403167'
+    #lon = '22.9352716'
+    #api_key = '35d0d7526bceb84756b29a9ece8b28f3'
+    
     is_weather_api_ready = HttpSensor(
         task_id="API_readiness_check_call",
         http_conn_id='weathermap_api',
-        endpoint='/data/3.0/onecall?lat={lat}&lon={lon}&appid={api_key}'
+        endpoint=f'/data/2.5/forecast?lat=40.6403167&lon=22.9352716&appid=35d0d7526bceb84756b29a9ece8b28f3'
+    )
+
+    extract_weather_data = SimpleHttpOperator(
+        task_id='extract_weather_data',
+        http_conn_id='weathermap_api',
+        endpoint=f'/data/2.5/forecast?lat=40.6403167&lon=22.9352716&appid=35d0d7526bceb84756b29a9ece8b28f3',
+        method='GET',
+        response_filter=lambda r: json.loads(r.text),
+        log_response=True
     )
     
+    is_weather_api_ready >> extract_weather_data
